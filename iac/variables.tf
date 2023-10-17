9047a3615ef93f04f6cdb10164a871c5ddff7b0e
franky-lim24 <56810823+Franky-Lim24@users.noreply.github.com>
2023-10-18 03:19:05 +0800

    add networking module

---
+++ iac/variables.tf
@@ -0,0 +1,5 @@
+variable "profile" {
+  description = "The profile for AWS CLI"
+  type        = string
+  default     = "default"
+}
---
+++ modules/networking.tf
@@ -0,0 +1,39 @@
+
+# Create a VPC
+resource "aws_vpc" "default-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "franky vpc"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "default_igw" {
+  vpc_id = aws_vpc.default-vpc.id
+}
+
+# Create a Subnet
+resource "aws_subnet" "default_subnet" {
+  vpc_id     = aws_vpc.default_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "franky Subnet"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "default_rt" {
+  vpc_id = aws_vpc.default_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default_igw.id
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "default_rta" {
+  subnet_id      = aws_subnet.default_subnet.id
+  route_table_id = aws_route_table.default_rt.id
+}
